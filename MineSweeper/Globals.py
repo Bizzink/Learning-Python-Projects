44a23403a932e20bd2794ee264ca4532a756e597
Bizink <58650321+Bizzink@users.noreply.github.com>
2019-12-15 19:43:15 +1300

    Minesweeper update 3

--- MineSweeper/Globals.py
+++ MineSweeper/Globals.py
@@ -1,15 +1,119 @@
+from time import time
+from Tile import Tile
+from random import randint
+import UI
+import pygame as pg
+
+#  Variables
+
+
 width = 0
 height = 0
 controls_height = 0
 rows = 0
 cols = 0
 tiles = []
-buttons = []
 screen = None
-clock = None
-flags_remaining = None
-settings_open = False
+first_click = True
 state = "PLAYING"
+flags_remaining = 0
 pause_time = 0
 flag_count = 0
 bomb_count = 0
+
+#  Functions
+
+
+def create_tiles(rows, cols):
+    """Creates array of tiles of size rows x cols"""
+    tile_array = []
+
+    for row in range(rows):
+        tile_row = []
+        for col in range(cols):
+            tile_row.append(Tile(col, row, int(width / max(cols, rows)), controls_height))
+        tile_array.append(tile_row)
+
+    return tile_array
+
+
+def spread_bombs(count, excluded_tile):
+    """Randomly spreads (count) bombs on the board, excluded_tile and the 8 surrounding tiles will not contain bombs"""
+    excluded_tiles = excluded_tile.get_neighbours()
+    excluded_tiles.append(excluded_tile)
+
+    while count > 0:
+        chosen_tile = tiles[randint(0, rows - 1)][randint(0, cols - 1)]
+        if not chosen_tile.bomb and chosen_tile not in excluded_tiles:
+            chosen_tile.bomb = True
+            count -= 1
+
+
+def tiles_left():
+    """Counts the number of un-revealed tiles left"""
+    remaining = 0
+
+    for row in tiles:
+        for tile in row:
+            if not tile.revealed: remaining += 1
+
+    return remaining
+
+
+def game_over(text, colour = (255, 255, 255)):
+    """Displays a game over title on the screen and sets the game state to paused"""
+    global state
+
+    for row in tiles:
+        for tile in row:
+            if tile.flagged: tile.toggle_flag()
+            tile.reveal()
+
+    overlay = pg.Surface((width, height - controls_height))
+    overlay.set_alpha(100)
+    overlay.fill((0, 0, 0))
+    screen.blit(overlay, (0, controls_height))
+
+    font = pg.font.Font("Gotham_Black.ttf", width // 5)
+    title = font.render(text, True, colour)
+    title_rect = title.get_rect()
+    title_rect.center = (width // 2, height // 2)
+    screen.blit(title, title_rect)
+
+    state = "PAUSED"
+
+
+def restart():
+    """Resets the board and game state"""
+    global first_click, tiles, state
+
+    tiles.clear()
+    UI.init()
+    tiles = create_tiles(rows, cols)
+    first_click = True
+    state = "PLAYING"
+
+
+def menu():
+    """Displays the menu"""
+    global state, pause_time
+
+    background = pg.Rect(0, 0, width // 3, height // 4)
+    background.center = (width // 2, height // 2)
+    pg.draw.rect(screen, (150, 150, 150), background)
+
+    for button in UI.menu_buttons:
+        button.draw()
+
+    pause_time = time()
+    state = "PAUSED"
+
+
+def close_menu():
+    global state
+    for row in tiles:
+        for tile in row:
+            tile.update()
+
+    UI.timer.start_time += (time() - pause_time)
+    state = "PLAYING"
--- MineSweeper/MineSweeper.py
+++ MineSweeper/MineSweeper.py
@@ -1,93 +1,25 @@
 from sys import exit, setrecursionlimit
-from random import randint
 from time import time
 import pygame as pg
-from Tile import Tile
 import Globals as g
 import UI
 
-
-def create_tiles(rows, cols):
-    tile_array = []
-    for row in range(rows):
-        tile_row = []
-        for col in range(cols):
-            tile_row.append(Tile(col, row, int(g.width / max(cols, rows)), g.controls_height))
-        tile_array.append(tile_row)
-
-    return tile_array
-
-
-def spread_bombs(count, excluded_tile):
-    excluded_tiles = excluded_tile.get_neighbours()
-    excluded_tiles.append(excluded_tile)
-
-    while count > 0:
-        chosen_tile = g.tiles[randint(0, g.rows - 1)][randint(0, g.cols - 1)]
-        if not chosen_tile.bomb and chosen_tile not in excluded_tiles:
-            chosen_tile.bomb = True
-            count -= 1
-
-
-def tiles_left():
-    #  count number of remaining hidden tiles
-    remaining = 0
-    for row in g.tiles:
-        for tile in row:
-            if not tile.revealed: remaining += 1
-
-    return remaining
-
-
-def game_over(text):
-    for row in g.tiles:
-        for tile in row:
-            if tile.flagged: tile.toggle_flag()
-            tile.reveal()
-
-    overlay = pg.Surface((g.width, g.height - g.controls_height))
-    overlay.set_alpha(100)
-    overlay.fill((0, 0, 0))
-    g.screen.blit(overlay, (0, g.controls_height))
-
-    font = pg.font.Font("Gotham_Black.ttf", g.width // 5)
-    title = font.render(text, True, (200, 255, 200))
-    title_rect = title.get_rect()
-    title_rect.center = (g.width // 2, g.height // 2)
-    g.screen.blit(title, title_rect)
-
-    g.state = "PAUSED"
-
-
-def restart():
-    global first_click
-    g.tiles.clear()
-    first_click = True
-    UI.init()
-    g.tiles = create_tiles(g.rows, g.cols)
-
-    g.state = "PLAYING"
-
-
+g.rows = 20
+g.cols = 20
+g.bomb_count = 50
+g.controls_height = 50
 setrecursionlimit(1500)
+
 pg.init()
 
 screen_size = g.width, g.height = 900, 950
 g.screen = pg.display.set_mode(screen_size)
 pg.display.set_caption("Minesweeper")
 
-
-g.rows = 20
-g.cols = 20
-g.bomb_count = 50
-g.flag_count = 0
-g.controls_height = 50
-first_click = True
-playing = True
-freeze = False
 UI.init()
-g.tiles = create_tiles(g.rows, g.cols)
+g.tiles = g.create_tiles(g.rows, g.cols)
 
+playing = True
 
 while True:
     for event in pg.event.get():
@@ -97,54 +29,41 @@ while True:
         if event.type == pg.MOUSEBUTTONDOWN:
             mouse_pos = pg.mouse.get_pos()
 
-            for button in g.buttons:
-                if button.rect.collidepoint(mouse_pos):
-                    button.action()
-                    freeze = True
+            if UI.menu.rect.collidepoint(mouse_pos):
+                UI.menu.action()
 
-            if not freeze and not g.settings_open:
+            if g.state == "PLAYING":
                 for row in g.tiles:
                     for tile in row:
                         if tile.rect.collidepoint(mouse_pos):
 
                             #  left click
                             if event.button == 1:
-                                if first_click:
-                                    spread_bombs(g.bomb_count, tile)
+                                if g.first_click:
+                                    g.spread_bombs(g.bomb_count, tile)
                                     tile.reveal()
-                                    g.clock.start_time = time()
-                                    first_click = False
+                                    UI.timer.start_time = time()
+                                    g.first_click = False
                                 elif not tile.revealed:
                                     tile.reveal()
-                                    if tile.bomb and not tile.flagged: game_over("LOSE!")
+                                    if tile.bomb and not tile.flagged: g.game_over("LOSE!")
 
                             #  right click
                             elif event.button == 3:
-                                if not tile.revealed: tile.toggle_flag()
+                                if not tile.revealed and not g.first_click: tile.toggle_flag()
                                 g.flags_remaining.update()
-
-                freeze = False
 
         if event.type == pg.KEYDOWN:
             #  Enter
-            if event.key == 13 and not g.state == "PLAYING": restart()
+            if event.key == 13 and not g.state == "PLAYING": g.restart()
 
             #  Escape
-            if event.key == 27:
-                if g.settings_open:
-                    for row in g.tiles:
-                        for tile in row:
-                            tile.update()
-                    g.settings_open = False
-                    g.state = "PLAYING"
-                    g.clock.start_time += (time() - g.pause_time)
-                else:
-                    UI.settings()
+            if event.key == 27: UI.menu.action()
 
-    if g.state == "PLAYING" and not first_click:
-        g.clock.update()
+    if g.state == "PLAYING" and not g.first_click:
+        UI.timer.update()
 
-    if tiles_left() == g.bomb_count:
-        game_over("WIN!")
+    if g.tiles_left() == g.bomb_count:
+        g.game_over("WIN!")
 
     pg.display.flip()
